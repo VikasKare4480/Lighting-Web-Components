5a42e08b01cfb9b47d9eca828d416dde5164db87
VikasKare <karevikas7777@gmail.com>
2026-08-02 19:16:37 +0530

    Create Record Adaptor

--- force-app/main/default/lwc/ldsCreateUiRecordApi/ldsCreateUiRecordApi.js
+++ force-app/main/default/lwc/ldsCreateUiRecordApi/ldsCreateUiRecordApi.js
@@ -1,4 +1,57 @@
 import { LightningElement } from 'lwc';
-import 
+import { createRecord } from 'lightning/uiRecordApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import FIRSTNAME_FIELD from "@salesforce/schema/Contact.FirstName";
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 
-export default class LdsCreateUiRecordApi extends LightningElement {}
+
+export default class LdsCreateUiRecordApi extends LightningElement {
+
+    fieldsContact = {};
+    fieldsAccount = {};
+
+    handleOnchangeAccount(event) {
+        event.preventDefault(); 
+        let {name, value} = event.target; 
+        this.fieldsAccount[name] = value;
+        // this.fieldsAccount = { ...this.fieldsAccount, [name]: value };
+        console.log('fields Account-->> ', JSON.stringify(this.fieldsAccount));
+    }
+
+    handleOnchangeContact(event) {
+        event.preventDefault(); 
+        let {name, value} = event.target; 
+        this.fieldsContact[name] = value;
+        // this.fieldsAccount = { ...this.fieldsAccount, [name]: value };
+        console.log('fields Contact-->> ', JSON.stringify(this.fieldsContact));
+    }
+
+
+    handleCreateRecord(event) {
+        event.preventDefault();
+
+        let accountFields = this.fieldsAccount;
+       
+        const inputRecord = {
+            apiName: ACCOUNT_OBJECT.objectApiName,
+            fields: {
+                [NAME_FIELD.fieldApiName]: this.fieldsAccount.Name
+            }
+        };
+
+        createRecord(inputRecord) // returns the promise
+        .t
+            console.log(JSON.stringify(result));
+        })
+        .catch((error) => {
+            console.log(JSON.stringify(error));
+        })
+        .finally(() => {
+            console.log(
+                'finally called'
+            );
+        });
+    }
+}
