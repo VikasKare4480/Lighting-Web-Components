0400dfa324064fc71815434feaabce3685be96a2
VikasKare <karevikas7777@gmail.com>
2026-08-04 21:17:52 +0530

    LDS Delete Record

--- force-app/main/default/lwc/ldsDeleteRecord/ldsDeleteRecord.js
+++ force-app/main/default/lwc/ldsDeleteRecord/ldsDeleteRecord.js
@@ -9,10 +9,10 @@ export default class LdsDeleteRecord extends LightningElement {
         deleteRecord(this.recordId)
         .then( (response) => {
             console.log('response -> ' + JSON.stringify(response));
-            this.showToast('Success', 'Record Deleted Successfully', 'success')    
+            this.showToast('Success ', 'Record Deleted Successfully', 'success')    
         }) 
         .catch( (error) => {
-            this.showToast('Error', 'Record Deleted UnSuccessfull', 'error')    
+            this.showToast('Error', 'Please Check Related Records', 'error')    
         })
         .finally( () => {
             console.log('Finally Called');
